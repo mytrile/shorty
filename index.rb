afc2ade3cd29f04ca19a3f3f7f373a74260382a7
Mitko Kostov <mitko.kostov@gmail.com>
2009-03-17 10:07:39 +0200

    first commit

---
+++ index.rb
@@ -0,0 +1,63 @@
+# Author  : Mitko Kostov
+# Email   : mitkok@7thoughts.com
+# Weblog  : http://7thoughts.com/mitko
+# License : MIT license
+
+# The default DB has one table named "data" with fields "url"  and "surl".
+
+require 'rubygems'
+require 'sinatra'
+require 'sequel'
+
+#HOST is your domain
+HOST = 'http://localhost:4567/' unless defined? HOST
+
+# Default page, gets the last five records
+get '/' do
+  get_record(5)
+  haml :index
+end
+
+# This function redirect the "shortened" url
+get %r{/([A-Z]+)} do
+  if @data = get_record(params[:captures])
+    redirect @data[:url]
+  else
+    "URL not found !"
+  end
+end
+
+# This function saves posted url
+post '/' do
+  shorten_url
+  get_record(1)
+  haml :index
+end
+
+private 
+
+def link_to (name, url, target="_blank")
+  "<a href="+url+" target=\""+target+"\">"+name+"</a>"
+end
+
+# This is the function that will shorten and record the posted url
+def shorten_url
+  db = Sequel.connect('sqlite://db/shorty.db')
+  data = db[:data]
+
+  if (data.order(:id).last) 
+    data.insert(:url => params[:url], :surl => data.order(:id).last[:surl].succ)
+  else
+    data.insert(:url => params[:url], :surl => "A")
+  end
+end
+
+# This fucntion gets record/s from the DB
+def get_record(param)
+  db = Sequel.connect('sqlite://db/shorty.db')
+  if param.is_a?(Integer)
+    @data = db[:data].limit(param).order(:id.desc).all
+  else
+    @data = db[:data].filter(:surl => param).first
+  end
+end
